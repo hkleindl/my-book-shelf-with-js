e6477bd19b7bb741da83c96632fb8e7234152b51
hkleindl <hkleindl@gmail.com>
2018-05-25 10:58:27 -0400

    add genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_145602) do
+ActiveRecord::Schema.define(version: 2018_05_25_145749) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_05_25_145602) do
   end
 
   create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
