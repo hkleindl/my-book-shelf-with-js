e08293c1e4a242227c5de9c41bbca675bf58fe34
hkleindl <hkleindl@gmail.com>
2018-06-01 11:56:37 -0400

    Add user_params

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :ratings
   has_many :books, through: :ratings
   has_secure_password
-
+  
   validates :username, presence: true, 
                        length: { maximum: 50 },
                        uniqueness: true
