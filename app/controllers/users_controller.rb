85e0c0aefda2f33518a5ba82bc9a5b4688294b29
hkleindl <hkleindl@gmail.com>
2018-06-01 12:12:38 -0400

    Fill in User create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{@user.username}!"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   private
