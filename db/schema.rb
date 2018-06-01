58e568080c0da329da780d37f962181cdad14bdf
hkleindl <hkleindl@gmail.com>
2018-06-01 11:36:07 -0400

    Add email attribute to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_163044) do
+ActiveRecord::Schema.define(version: 2018_06_01_153525) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_05_30_163044) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
