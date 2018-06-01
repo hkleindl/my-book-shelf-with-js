762c1e532a0e13ee39827d9b57652e966740db9f
hkleindl <hkleindl@gmail.com>
2018-06-01 11:49:37 -0400

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :ratings
   has_many :books, through: :ratings
   has_secure_password
+
+  validates :username, presence: true, 
+                       length: { maximum: 50 },
+                       uniqueness: true
+  validates :email, presence: true, 
+                    length: { maximum: 100 },
+                    uniqueness: true
+  validates :password, presence: true,
+                       length: { in: 6..20 }
 end
