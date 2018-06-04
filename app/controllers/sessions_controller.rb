5eaf31b3d5f8c13e09644b78252e6e330ad276de
hkleindl <hkleindl@gmail.com>
2018-06-04 12:16:36 -0400

    Allow user to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
+
   end
 
   def create
-    
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(:password)
+      login(@user)
+      redirect_to @user
+      flash[:success] = "Welcome Back, #{current_user.username}!"
+    else
+      render 'static_pages/home'
+    end
   end
 end
