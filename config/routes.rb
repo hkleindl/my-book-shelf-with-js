0b959fba8b956f4a195bec26bee07f09bfc0505e
hkleindl <hkleindl@gmail.com>
2018-06-06 09:26:12 -0400

    Add nested books resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show] do
+    resources :books, only: [:index, :show]
+  end
+  
   resources :sessions, only: [:create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
