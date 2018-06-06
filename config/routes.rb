72d0d5a9f1e1d6d9cd84eeb02dfe6a0349823416
hkleindl <hkleindl@gmail.com>
2018-06-06 09:27:31 -0400

    Add books resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show] do
     resources :books, only: [:index, :show]
   end
-  
+  resources :books
   resources :sessions, only: [:create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
