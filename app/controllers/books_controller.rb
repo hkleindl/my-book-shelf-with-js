f4499e5f8bc06e4bc166e55d38f9d84713939511
hkleindl <hkleindl@gmail.com>
2018-06-06 10:34:48 -0400

    Add id's to ratings table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
   def index
-    
+    if current_user
+      @books = current_user.books
+    else
+      @books = Book.all
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_153525) do
+ActiveRecord::Schema.define(version: 2018_06_06_143350) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_06_01_153525) do
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
