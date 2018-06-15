03aaf149d0eb62b6c1811c5b62e60b0b9bef6462
hkleindl <hkleindl@gmail.com>
2018-06-15 09:51:00 -0400

    Add Author routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
   resources :books
   resources :sessions, only: [:create]
+  resources :authors, only: [:index, :show]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
