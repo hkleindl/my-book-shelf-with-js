a41c50fedf17a12617655fc0f4cbccf2c71275c7
hkleindl <hkleindl@gmail.com>
2018-07-09 12:51:56 -0400

    Add book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
   has_many :genres, through: :book_genres
 
   validates :name, :author_name, presence: true
+  validates :name, uniqueness: { scope: :author, message: "Book name by that author already exists." }
 
   accepts_nested_attributes_for :ratings
   accepts_nested_attributes_for :genres
