878460cfd07d9417d7bf18efd26f47192f763e3a
hkleindl <hkleindl@gmail.com>
2018-07-09 13:58:31 -0400

    Add edit, update route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :authors, only: [:index, :show]
   resources :genres, only: [:index, :show]
-  resources :ratings, only: [:new, :create]
+  resources :ratings, only: [:new, :create, :edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
