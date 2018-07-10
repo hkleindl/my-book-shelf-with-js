1949d934e72d2681438eb195c98cbdc1affa5035
hkleindl <hkleindl@gmail.com>
2018-07-10 11:56:17 -0400

    Add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :authors, only: [:index, :show]
   resources :genres, only: [:index, :show]
-  resources :ratings, only: [:new, :create, :edit, :update]
+  resources :ratings, only: [:new, :create, :edit, :update, :delete]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
