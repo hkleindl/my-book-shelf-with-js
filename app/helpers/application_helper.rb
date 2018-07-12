daab76514b23f71c9d967f756283bdd48bec6b19
hkleindl <hkleindl@gmail.com>
2018-07-12 15:35:06 -0400

    Fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def login_page?
+    !!current_page?(root_path)  
+  end
+
+  def signup_page?
+    params[:controller] == "users" && (params[:action] == "new" || params[:action] == "create")
+  end
+
 end
