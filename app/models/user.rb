7b5a9acea8e933cabc596a3e24df7216c5aabe6b
hkleindl <hkleindl@gmail.com>
2018-09-03 14:29:32 -0400

    Refactor validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :email, presence: true, 
                     length: { maximum: 100 },
                     uniqueness: true
-  validates :password, length: { minimum: 6 },
-                       allow_nil: true
+  validates :password, length: { minimum: 6 }, on: :create
+                      #  allow_nil: true
 
   # validates :password, presence: true,
   #                      length: { in: 6..20 }                     
