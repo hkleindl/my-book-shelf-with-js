6bc7908e914e5b67a91afa48b95bbb193987028a
hkleindl <hkleindl@gmail.com>
2019-01-29 10:27:04 -0500

    Refactor

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -5,6 +5,26 @@ function Book(attributes) {
   this.averageStarRating = attributes.average_star_rating
 }
 
+function showDetails(bookId, listName) {
+  showStuff(bookId, listName);
+}
+
+// function currentUserRating(bookId, listName) {
+//   $.getJSON(`/books/${bookId}`, function(resp) {
+//     var rating = resp.ratings.filter(function(r) {
+//       return r.user_id === $('#user-id').data('user-id')
+//     })
+//     debugger
+//     $(`#${listName}-${bookId}`).html(`My Rating: ${rating[0].stars}`).toggle(100, "swing")
+//     alert(rating[0].stars)
+//   })
+// }
+
+// $.getJSON('/books/1', function(r) {
+//   r1 = r.ratings.filter(e => e.user_id === 1)
+// })
+// r1[0]
+
 function showStuff(bookId, listName) {
   $.getJSON(`/books/${bookId}`, function(resp) {
     $(`#${listName}-${bookId}`).html(
