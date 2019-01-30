9e521332ff88f2699892d0503b364198209af053
hkleindl <hkleindl@gmail.com>
2019-01-30 12:05:20 -0500

    Sift through Book's ratings

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -1,30 +1,35 @@
-var json = $.getJSON('/books/1')
-var ratingsList = json.responseJSON.ratings
+$(function() {
+  var myJson; 
+  $.getJSON('/books/1', function(r) {
+    myJson = r;
+  
+    var ratingsList = myJson.ratings
 
-var i = 0
+    var i = 0
+    $('#list').html(`${ratingsList[0].user_name} - ${ratingsList[0].stars} stars`)
 
-$('#list').html(ratingsList[0])
+    $('#prev').on('click', function(e) {
+      e.preventDefault;
+      $('#list').html(prevItem())
+    })
 
-$('#prev').on('click', function(e) {
-  e.preventDefault;
-  $('#list').html(prevItem())
-})
+    $('#next').on('click', function(e) {
+      e.preventDefault;
+      $('#list').html(nextItem())
+    })
 
-$('#next').on('click', function(e) {
-  e.preventDefault;
-  $('#list').html(nextItem())
-})
+    function prevItem() {
+      if (i === 0) {
+        i = ratingsList.length;
+      }
+      i--;
+      return `${ratingsList[i].user_name} - ${ratingsList[i].stars} stars`
+    }
 
-function prevItem() {
-  if (i === 0) {
-    i = ratingsList.length;
-  }
-  i--;
-  return ratingsList[i]
-}
-
-function nextItem() {
-  i++;
-  i = i % ratingsList.length
-  return ratingsList[i]
-}
+    function nextItem() {
+      i++;
+      i = i % ratingsList.length
+      return `${ratingsList[i].user_name} - ${ratingsList[i].stars} stars`
+    }
+  })
+});
